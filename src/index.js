eba84ab6243e7355bcdbff05782db05dcb9f0412
Luis Alfonso Alarcon <45520382+alfonso2254@users.noreply.github.com>
2023-12-04 15:07:06 -0400

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,111 +1,106 @@
 const axios = require("axios");
-const { io } = require('socket.io-client');
-
-
-
-
+const { io } = require("socket.io-client");
 
 class ChatBc {
-    constructor({ url, tokenClient}) {
-        this.url = url;
-        this.tokenClient = tokenClient; 
-        
-        this.customer = {};    
-        this.user = {};    
-        this.channelsData = [];
-        
+  constructor({ url, tokenClient }) {
+    this.url = url;
+    this.tokenClient = tokenClient;
 
-        this.socketId = null;
-        this.socket = io(url,
-            {
-                transports: ['websocket']
-            }
-        );
+    this.customer = {};
+    this.user = {};
+    this.channelsData = [];
 
-        this.socket.on('connect', (socket) => {
-            console.log('Conectado', socket.id);
-            
-            this.socketId = socket.id;
-        });
-    }
+    this.socketId = null;
+    this.socket = io(url, {
+      transports: ["websocket"],
+    });
 
-    async getMessagesByChannel({username , channel}) {
-        try {
-            const { data } = await axios({
-                url: `${this.url}/api/messages-by-channel`,
-                method: "get",
-                params: {
-                    username,
-                    channel,
-                    tokenClient: this.tokenClient
-                }
-            });
+    this.socket.on("connect", (socket) => {
+      console.log("Conectado", socket.id);
 
-            this.customer = data.customer;
-            this.user = data.user;
+      this.socketId = socket.id;
+    });
+  }
 
-            if(this.channelsData.findIndex(item => item.channel === channel) === -1) {
-                this.channelsData.push(data.channel);
-            }
-            
-            return data;
-        } catch (error) {
-            return error;
-        }
-    }
+  async getMessagesByChannel({ username, channel }) {
+    try {
+      const { data } = await axios({
+        url: `${this.url}/api/messages-by-channel`,
+        method: "get",
+        params: {
+          username,
+          channel,
+          tokenClient: this.tokenClient,
+        },
+      });
 
+      this.customer = data.customer;
+      this.user = data.user;
 
-    async sendMessages({ message, channel }) {
-        console.log({ message, channel }, 'sendMessages');
-        const channelId = this.channelsData.find(item => item.channel === channel)?._id || null;
-        const customerId = this.customer._id || null;
-        try {
-            const { data } = await axios({
-                url: `${this.url}/api/send-messages`,
-                method: "post",
-                data: {
-                    message,
-                    userId: this.user._id,
-                    channelId: channelId,
-                    customerId: customerId,
-                },
-                headers: {
-                    // socketId: socket.id,
-                    socketId: '123',
-                }
-            });
-            return data;
-        }catch (error) {
-            return error;
-        }
-    }
+      if (
+        this.channelsData.findIndex((item) => item.channel === channel) === -1
+      ) {
+        this.channelsData.push(data.channel);
+      }
 
-    onMessages(channel, callback) {
-        this.socket.on('connect', () => {
-            console.log('Conectado');
-        });
-
-        const channelId = this.channelsData.find(item => item.channel === channel)?._id || null;
-        console.log('Conectado al canal:', `channel_message_${channelId}`);
-        this.socket.on(`channel_message_${channelId}`, (data) => {
-            callback(data);
-        });
-
-        this.socket.on('error', (error) => {
-            console.error(error);
-        });
-    }
-
-    getDataConversation() {
-        return {
-            customer: this.customer,
-            user: this.user,
-            channelsData: this.channelsData,
-            tokenClient: this.tokenClient || "not token client",
-            url: this.url
-        }
+      return data;
+    } catch (error) {
+      return error;
     }
   }
-  
-  module.exports = ChatBc;
-  
+
+  async sendMessages({ message, channel }) {
+    console.log({ message, channel }, "sendMessages");
+    const channelId =
+      this.channelsData.find((item) => item.channel === channel)?._id || null;
+    const customerId = this.customer._id || null;
+    try {
+      const { data } = await axios({
+        url: `${this.url}/api/send-messages`,
+        method: "post",
+        data: {
+          message,
+          userId: this.user._id,
+          channelId: channelId,
+          customerId: customerId,
+        },
+        headers: {
+          // socketId: socket.id,
+          socketId: "123",
+        },
+      });
+      return data;
+    } catch (error) {
+      return error;
+    }
+  }
+
+  onMessages(channel, callback) {
+    this.socket.on("connect", () => {
+      console.log("Conectado");
+    });
+
+    const channelId =
+      this.channelsData.find((item) => item.channel === channel)?._id || null;
+    console.log("Conectado al canal:", `channel_message_${channelId}`);
+    this.socket.on(`channel_message_${channelId}`, (data) => {
+      callback(data);
+    });
+
+    this.socket.on("error", (error) => {
+      console.error(error);
+    });
+  }
+
+  getDataConversation() {
+    return {
+      customer: this.customer,
+      user: this.user,
+      channelsData: this.channelsData,
+      tokenClient: this.tokenClient || "not token client",
+      url: this.url,
+    };
+  }
+}
+
+module.exports = ChatBc;
